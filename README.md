12a95865a18cfc163430c47ae8f8078c05ae9390
royweb <xytank@gmail.com>
2020-10-26 13:20:46 -0400

    Install lite-server for BrowserSync

---
+++ README.md
@@ -0,0 +1,9 @@
+# Blockchain Todo list
+
+## Note
+- Deploying smart contracts to the blockchain costs gas
+- `truffle mirgate` deploy a smart contract to the blockchain
+- `truffle mirgate --reset` deploy a new copy of the smart contract to the blockchain
+
+## Dependencies
+- `lite-server` for BrowserSync
--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.0;
+
 contract TodoList {
     // State variables
     // public keyword create a function to access to this variable
@@ -20,7 +21,7 @@ contract TodoList {
     // mapping is like an associative array or a hash
     // uint will refer to the id of the task
     mapping(uint => Task) public tasks;
-    
+
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
