3f6041257218bc5ff3e66ec51bad09a8df12d1dc
royweb <xytank@gmail.com>
2020-10-29 14:12:13 -0400

    Test the createTask()

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -1,3 +1,5 @@
+const { assert } = require("chai");
+
 const TodoList = artifacts.require('./TodoList.sol');
 
 contract('TodoList', (accounts) => {
@@ -24,5 +26,17 @@ contract('TodoList', (accounts) => {
         assert.equal(task.content, 'Buy Candy');
         assert.equal(task.completed, false);
         assert.equal(taskCount.toNumber(), 1);
+    })
+
+    it('creates tasks', async () => {
+        const result = await this.todoList.createTask('Clean the sink');
+        const taskCount = await this.todoList.taskCount();
+        assert.equal(taskCount, 2);
+
+        // Test the event log
+        const event = result.logs[0].args;
+        assert.equal(event.id.toNumber(), 2);
+        assert.equal(event.content, 'Clean the sink');
+        assert.equal(event.completed, false);
     })
 })
