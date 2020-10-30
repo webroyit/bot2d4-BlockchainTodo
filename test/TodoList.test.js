777699d1c10ec10eecd8d8259b8e4aad0b2d8aa2
royweb <xytank@gmail.com>
2020-10-30 13:36:41 -0400

    Test toggleCompleted()

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -39,4 +39,15 @@ contract('TodoList', (accounts) => {
         assert.equal(event.content, 'Clean the sink');
         assert.equal(event.completed, false);
     })
+
+    it('toggles task completion', async () => {
+        const result = await this.todoList.toggleCompleted(1);
+        const task = await this.todoList.tasks(1);
+        assert.equal(task.completed, true);
+
+        // Test the event log
+        const event = result.logs[0].args;
+        assert.equal(event.id.toNumber(), 1);
+        assert.equal(event.completed, true);
+    })
 })
